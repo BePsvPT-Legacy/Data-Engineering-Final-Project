2654415051285dbb388f998a254407462593e4cf
BePsvPT <freedom@csie.io>
2016-06-21 10:51:45 +0800

    Fix large file crash

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,8 @@
 <?php
 
+define('BUFFER_SIZE', 1024 * 1024);
+define('FREAD_SIZE', 1024 * 128);
+
 function packData($type, $data = null)
 {
     return serialize([
@@ -13,19 +16,37 @@ function unpackData($data)
     return unserialize(trim($data));
 }
 
-function indexDir($path)
+function indexDir($dir)
 {
     $filesystem = new \Illuminate\Filesystem\Filesystem();
 
-    $files = ['path' => [], 'hash' => []];
+    $files = [];
 
-    foreach ($filesystem->allFiles($path) as $file) {
+    foreach ($filesystem->allFiles($dir) as $file) {
         /** @var Symfony\Component\Finder\SplFileInfo $file */
+        $hash = sha1(implode('|', [
+            $file->getRelativePathname(), $file->getType(), $file->getSize(), $file->getMTime(),
+        ]));
 
-        $files['path'][$file->getRelativePathname()] = md5_file(file_build_path($path, $file->getRelativePathname()));
+        $path = file_build_path($dir, $file->getRelativePathname());
+
+        $fp = fopen($path, "rb");
+        $first100 = fread($fp, 100);
+        fseek($fp, -100, SEEK_END);
+        $last100 = fread($fp, 100);
+        fclose($fp);
+
+        $files[$hash] = [
+            'path' => $file->getRelativePathname(),
+            'dir' => $file->getRelativePath(),
+            'name' => $file->getFilename(),
+            'type' => $file->getType(),
+            'size' => $file->getSize(),
+            'mtime' => $file->getMTime(),
+            'mime' => mime_content_type($path),
+            'quickHash' => md5("{$first100}|{$last100}"),
+        ];
     }
-
-    $files['hash'] = array_flip($files['path']);
 
     return $files;
 }
@@ -33,4 +54,11 @@ function indexDir($path)
 function file_build_path(...$segments)
 {
     return implode(DIRECTORY_SEPARATOR, $segments);
+}
+
+function create_dir ($path)
+{
+    if (strlen($path) > 0 && ! is_dir($path)) {
+        mkdir($path, 0755, true);
+    }
 }
