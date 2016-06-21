68fed1d97bafeb60ac7ea5d00ade6c43f6c90f14
DanSnow <dododavid006@gmail.com>
2016-06-21 15:43:03 +0800

    fix(server): Fix bug

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
+import fs from 'fs'
 import express from 'express'
+import http from 'http'
 import Socket from 'socket.io'
+import { indexDir } from './helpers'
 
-const app = express.createServer()
-const io = new Socket(app)
+const app = express()
+const server = http.Server(app)
+const io = new Socket(server)
 
-app.listen(32123)
+io.on('connection', (socket) => {
+  console.log('New connection')
+  indexDir().then((data) => {
+    socket.emit('index', data)
+  })
+
+  socket.on('index', () => {
+    indexDir().then((data) => {
+      socket.emit('index', data)
+    })
+  })
+})
+
+server.listen(32123, '0.0.0.0')
+console.log('Server start')
